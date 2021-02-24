b3e94e3257a18fa8dcdc218442bd0d6550a4f13c
vrknetha <vrknetha@gmail.com>
2021-02-24 15:35:47 +0530

    initial commit

---
+++ cypress/tests/e2e-tests/google.spec.ts
@@ -0,0 +1,29 @@
+describe(`google search test`, () => {
+  const searchKey = `Michael Jordan`;
+
+  beforeEach(() => {
+    cy.visit(`/`);
+  });
+
+  it(`search using text and verify field should have the value`, () => {
+    cy.search(searchKey);
+    cy.get(`input[title="Search"]`).should(`have.value`, searchKey);
+  });
+
+  it.only(`verify the number of results generated`, () => {
+    let length = 0;
+    cy.search(searchKey)
+      .get(`div[role="option"]`)
+      .each((element) => {
+        if (element.text() !== "") {
+          length += 1;
+          cy.wrap(element)
+            .contains(`michael`, { matchCase: false })
+            .contains(`jordan`, { matchCase: false });
+        }
+      })
+      .then(() => {
+        cy.wrap(length).should(`eq`, 10);
+      });
+  });
+});
